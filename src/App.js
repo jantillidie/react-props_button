f929fc85ca64989f016bb4980567281a795c2ad9
Jan Maaß <git@antipost.de>
2023-11-13 16:29:17 +0100

    Pass function as a prop

--- src/App.js
+++ src/App.js
@@ -2,5 +2,11 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  const handleClick = () => alert('You clicked me!');
+  return <Button color={"red"} disabled={false} text={"Red Button"} onClick={handleClick} />;
+}
+
+function Button({ color, disabled, text, onClick }) {
+
+  return <button style={{ height: "50px", width: "200px", backgroundColor: color, border: "none", fontSize: "1rem", color: "whitesmoke" }} disabled={disabled} onClick={onClick} >{text}</button>
 }
